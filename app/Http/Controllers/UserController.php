ccb84f5ae962b809e291f42c5d342d172854f8bc
Felipe <felipedesousa2001@gmail.com>
2023-03-10 23:05:17 -0300

    validando os dados do form update e salvando

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -55,11 +55,21 @@ class UserController extends Controller
 
         return view('users.edit', compact('user'));
     }
-    public function update(Request $request, $id)
+    public function update(StoreUpdateUserFormRequest $request, $id)
     {
         if (!$user = User::find($id))
             return redirect()->route('users.index');
 
-        dd($request->all());
+
+        // $user->name = $request->get('name');
+        // $user->save();
+
+        $data = $request->only(['name', 'email']);
+        if ($request->password)
+            $data['password'] = bcrypt($request->password);
+
+        $user->update($data);
+
+        return redirect()->route('users.index');
     }
 }
