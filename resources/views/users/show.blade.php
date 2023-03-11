5a5aab42a62122de617d91489ab33814c04137d1
Felipe <felipedesousa2001@gmail.com>
2023-03-10 23:11:30 -0300

    delete users

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -7,7 +7,12 @@
 
     <ul>
         <li>{{ $user->name }}</li>
-        <li>{{ $user->email }}</li>
-        <li>{{ $user->created_at }}</li>
+        <sli>{{ $user->email }}</li>
     </ul>
+
+    <form action="{{ route('users.delete', $user->id) }}" method="POST">
+        @method('DELETE')
+        @csrf
+        <button type="submit">Deletar</button>
+    </form>
 @endsection
